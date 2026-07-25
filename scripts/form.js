aa8646aaa9f8ee5036241b0ea54ca62e2f41ed38
Celi <celikmack@gmail.com>
2026-07-24 22:22:19 -0300

    Product Review Form

---
+++ scripts/form.js
@@ -0,0 +1,83 @@
+const spanYear = document.getElementById("currentYear")
+
+const today = new Date()
+const currentYear = today.getFullYear()
+
+spanYear.innerHTML = currentYear
+
+const lastModified = document.getElementById("lastModified")
+
+lastModified.innerHTML = `Last Modification: ${document.lastModified}`
+
+// Populate options
+
+const products = [
+  {
+    id: "fc-1888",
+    name: "flux capacitor",
+    averagerating: 4.5
+  },
+  {
+    id: "fc-2050",
+    name: "power laces",
+    averagerating: 4.7
+  },
+  {
+    id: "fs-1987",
+    name: "time circuits",
+    averagerating: 3.5
+  },
+  {
+    id: "ac-2000",
+    name: "low voltage reactor",
+    averagerating: 3.9
+  },
+  {
+    id: "jj-1969",
+    name: "warp equalizer",
+    averagerating: 5.0
+  }
+];
+
+function populateProduct() {
+  const select = document.querySelector("#product");
+  select.innerHTML = ""
+  
+  const placeholder = document.createElement("option");
+  placeholder.value = "";
+  placeholder.textContent = "Select a Product...";
+  placeholder.disabled = true;
+  placeholder.selected = true;
+  select.appendChild(placeholder);
+
+  products.forEach(product => {
+    const option = document.createElement("option");
+    option.value = product.id;
+    option.textContent = product.name;
+    select.appendChild(option);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const select = document.querySelector("#product");
+  const message = document.querySelector("#counterMessage");
+  const form = document.querySelector("form");
+
+  if (select) {
+    populateProduct();
+  }
+  
+  if (message) {
+  const reviewCounter = "review-count";
+  let count = Number(localStorage.getItem(reviewCounter)) || 0;
+  count += 1;
+  localStorage.setItem(reviewCounter, count);
+
+  let showUser = "You have submitted: " + count + " review"; 
+    if (count !== 1) {
+      showUser += "s";
+    }
+    showUser += "!";
+    message.textContent = showUser;
+    }
+});
